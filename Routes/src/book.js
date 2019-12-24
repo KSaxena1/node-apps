62edbc977c0e6fe3b4f26c6ef19577c4d279470e
KSaxena1 <ksstudyemail@gmail.com>
2019-12-24 14:12:11 -0500

    book route by id

--- Routes/src/book.js
+++ Routes/src/book.js
@@ -25,4 +25,30 @@ bookRouter.get("/list", (req, res) => {
   });
 });
 
+bookRouter.get("/list/:id", (req, res) => {
+  console.log(req.params.id);
+  sql.connect(config, function(err) {
+    if (err) console.log(err);
+
+    //create Request object
+    var request = new sql.Request();
+    console.log(req.params.id);
+    // query to the database and get the records
+    request.query(
+      `select * from [dbo].books where bookId = '${req.params.id}'`,
+      function(err, recordsets) {
+        if (err) {
+          console.log(err);
+          sql.close();
+        }
+        // send records as a response
+        res.setHeader("Content-Type", "application/json");
+        //console.log(recordsets.recordset);
+        res.send(JSON.stringify(recordsets.recordset));
+        sql.close();
+      }
+    );
+  });
+});
+
 module.exports = bookRouter;
