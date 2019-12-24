5fae215b7c3e626918c8063244e031d0eb60d231
KSaxena1 <ksstudyemail@gmail.com>
2019-12-24 12:40:17 -0500

    sql setup

--- SQLsetup.sql
+++ SQLsetup.sql
@@ -3,36 +3,78 @@
 USE [MASTER]
 create database [coffeenbooks];
 GO
+-----------------------------------------
 
 CREATE TABLE coffeenbooks.[dbo].[books]
 (
-    [bookId] [varchar](20) NOT NULL,
-    [title] [varchar](60) NULL,
-    [author] [varchar](60) NULL,
+    [bookId] [varchar](20) NOT NULL
+	,
+    [title] [varchar](60) NULL
+	,
+    [author] [varchar](60) NULL
+	,
     [rating] [smallint] NULL
-        PRIMARY KEY CLUSTERED 
-(
-	[bookId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	,
+    [qtyAtHand] [int] NOT NULL PRIMARY KEY CLUSTERED ([bookId] ASC) WITH (
+		PAD_INDEX = OFF
+		, STATISTICS_NORECOMPUTE = OFF
+		, IGNORE_DUP_KEY = OFF
+		, ALLOW_ROW_LOCKS = ON
+		, ALLOW_PAGE_LOCKS = ON
+		)
+	ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
 
 INSERT INTO coffeenbooks.[dbo].books
 VALUES
-    ('A1111', 'Moby Dick', 'Herman Melville', 10);
+    (
+        'A1111'
+	, 'Moby Dick'
+	, 'Herman Melville'
+	, 10
+	, 12
+	);
+
 INSERT INTO coffeenbooks.[dbo].books
 VALUES
-    ('A2222', 'Get Rich Really Fast', 'Ima Scammer', 1);
+    (
+        'A2222'
+	, 'Get Rich Really Fast'
+	, 'Ima Scammer'
+	, 1
+	, 4
+	);
+
 INSERT INTO coffeenbooks.[dbo].books
 VALUES
-    ('A3333', 'Finding Inner Peace', 'Serenity Blissford', null);
+    (
+        'A3333'
+	, 'Finding Inner Peace'
+	, 'Serenity Blissford'
+	, NULL
+	, 15
+	);
+
 INSERT INTO coffeenbooks.[dbo].books
 VALUES
-    ('A4444', 'Great Mystery Stories', 'Rodney Whodunit', 5);
+    (
+        'A4444'
+	, 'Great Mystery Stories'
+	, 'Rodney Whodunit'
+	, 5
+	, 10
+	);
+
 INSERT INTO coffeenbooks.[dbo].books
 VALUES
-    ('A5555', 'Software Wizardry', 'D. Abugov', 10);
+    (
+        'A5555'
+	, 'Software Wizardry'
+	, 'D. Abugov'
+	, 10
+	, 15
+	);
 
 
 
