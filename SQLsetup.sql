94feca2e6436073a71cfcb79e1636649f427907c
KSaxena1 <ksstudyemail@gmail.com>
2019-12-24 12:57:11 -0500

    sql coffee setup

--- SQLsetup.sql
+++ SQLsetup.sql
@@ -3,7 +3,8 @@
 USE [MASTER]
 create database [coffeenbooks];
 GO
------------------------------------------
+
+-- CREATE TABLE BOOKS FOR ALL THE TITLES WITH INVENTORY
 
 CREATE TABLE coffeenbooks.[dbo].[books]
 (
@@ -77,4 +78,67 @@ VALUES
 	);
 
 
+--CREATE A COFFEE DATABASE WITH PRICE AND INVENTORY
+CREATE TABLE [coffeenbooks].[dbo].[coffee]
+(
+    [coffeeId] [int] NOT NULL
+	,
+    [coffeeName] [varchar](30) NOT NULL
+	,
+    [pricePerCup] [decimal](4, 2) NOT NULL
+	,
+    [qtyAvailForCups] [int] NOT NULL PRIMARY KEY CLUSTERED ([coffeeId] ASC) WITH (
+		PAD_INDEX = OFF
+		, STATISTICS_NORECOMPUTE = OFF
+		, IGNORE_DUP_KEY = OFF
+		, ALLOW_ROW_LOCKS = ON
+		, ALLOW_PAGE_LOCKS = ON
+		)
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
+INSERT INTO [coffeenbooks].[dbo].[coffee]
+VALUES
+    (
+        1001
+	, 'Colombian'
+	, 7.99
+	, 10
+	);
+
+INSERT INTO [coffeenbooks].[dbo].[coffee]
+VALUES
+    (
+        1002
+	, 'French Roast'
+	, 8.99
+	, 10
+	);
+
+INSERT INTO [coffeenbooks].[dbo].[coffee]
+VALUES
+    (
+        1003
+	, 'Espresso'
+	, 9.99
+	, 10
+	);
+
+INSERT INTO [coffeenbooks].[dbo].[coffee]
+VALUES
+    (
+        1004
+	, 'Colombian Decaf'
+	, 8.99
+	, 10
+	);
+
+INSERT INTO [coffeenbooks].[dbo].[coffee]
+VALUES
+    (
+        1005
+	, 'French Roast Decaf'
+	, 9.99
+	, 10
+	);
